ec419d717a8e5330a5fd43727ba3a95da7d9f9ef
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-03-13 16:46:55 +0000

    Centre preloading bar

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -5,7 +5,7 @@ import config from '../config';
 export default class extends Phaser.State {
     preload() {
         this.stage.backgroundColor = '#225d6b';
-        this.preloadBar = this.add.sprite((config.gameWidth - 311) / 2, (config.gameHeight - 27) / 2, 'bars', 1);
+        this.preloadBar = this.add.sprite((config.gameWidth - 500) / 2, config.gameHeight / 2, 'bars', 1);
         this.preloadBar.anchor.setTo(0, 0.5);
         this.load.setPreloadSprite(this.preloadBar);
 
